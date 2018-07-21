69ba043921cd754db98828db07a24da5cf37843f
EmptyException <unityamvs@gmail.com>
2018-07-21 17:40:22 -0300

    Rewrite all

--- example.js
+++ example.js
@@ -1,21 +1,24 @@
-var cleverbot = require("./app.js");
-var readline = require('readline');
-var rl = readline.createInterface(process.stdin, process.stdout);
-bot = new cleverbot("YOUR_API_USER", "YOUR_API_KEY"); // cleverbot (user, key, nick)
-
-bot.setNick("YOUR_NICK"); // This is optional
-bot.create(function (err, response) {
-	rl.setPrompt('You> ');
-	rl.prompt();
-	rl.on('line', function(line) {
-		bot.ask(line, function (err, response) {
-			if (err) throw response;
-			console.log("Cleverbot:", response);
-			rl.prompt();
-		});
-	}).on('close',function(){
-		process.exit(0);
-	});
-});
+const cleverbot = require('./index.js');
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+const bot = new cleverbot('YOUR_USER_KEY', 'YOUR_API_KEY'); // cleverbot (user, key, nick)
+bot.create('test').then(() => {
+  rl.setPrompt('You> ');
+  rl.prompt();
+  rl.on('line', (line) => {
+    bot.ask(line).then((response) => {
+      console.log('Cleverbot:', response);
+      rl.prompt();
+    })
+      .catch((e) => {
+        console.error(e);
+      })
+  }).on('close', () => {
+    process.exit(0);
+  });
+})
+  .catch((e) => {
+    console.error(e);
+  });
 
 	
---
+++ index.js
@@ -0,0 +1,58 @@
+const request = require('superagent');
+const base_url = 'https://cleverbot.io/1.0';
+class CleverBot {
+  constructor(user, key, nick) {
+    this.user = user;
+    this.key = key;
+    this.nick = nick || null;
+  }
+
+  setNick(newNick) {
+    this.nick = newNick;
+  }
+
+  /**
+   * Creates a new clever bot session.
+   * @param {string} nick the cleverbot nickname
+   * @returns {Promise<void>} whether or not the session has been created.
+   */
+  create(nick) {
+    return new Promise((resolve, reject) => {
+      request.post(`${base_url}/create`).send({
+        user: this.user,
+        key: this.key,
+        nick
+      })
+        .then((req) => {
+          const nick = req.body.nick;
+          this.nick = nick;
+          resolve();
+        })
+        .catch((e) => reject(e.response.error));
+    });
+  }
+
+
+  /**
+   * Get a response from the clb api based in the user input.
+   * @param {string} input user input
+   * @returns {Promise<string>} the response from clb api.
+   */
+  ask(input) {
+    return new Promise((resolve, reject) => {
+      request.post(`${base_url}/ask`)
+        .send({
+          user: this.user,
+          key: this.key,
+          nick: this.nick,
+          text: input
+        })
+        .then((req) => {
+          resolve(req.body.response);
+        })
+        .catch((e) => reject(e.response.error))
+    });
+  }
+}
+
+module.exports = CleverBot;
